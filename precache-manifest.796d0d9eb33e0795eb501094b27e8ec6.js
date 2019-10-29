6ad40dffac3b474414cc805007d5c1334e01afb6
vishnu <vishnu@example.com>
2019-10-29 14:44:26 +0530

    Updates

---
+++ precache-manifest.796d0d9eb33e0795eb501094b27e8ec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409c51166616423d69a1233fdc1be45d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59b197c22280042a1a3d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c7bbfd37a21e9d735a49",
+    "url": "/static/js/2.aafba989.chunk.js"
+  },
+  {
+    "revision": "59b197c22280042a1a3d",
+    "url": "/static/js/main.ee3fc03b.chunk.js"
+  },
+  {
+    "revision": "bb6e324978c6b93739f5",
+    "url": "/static/js/runtime-main.e7b7af1d.js"
+  }
+]);
