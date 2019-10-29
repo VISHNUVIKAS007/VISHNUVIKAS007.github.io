28e6508d935d0784994ecfb206716a2c20084b07
vishnu <vishnu@example.com>
2019-10-29 14:05:28 +0530

    resume changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import resumeData from './resumeData';
+import Footer from './components/footer/footer';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header  resumeData={resumeData}/>
+        <About />
+        <Resume />
+        <Footer />
+      </div>
+    );
+  }
+}export default App;
